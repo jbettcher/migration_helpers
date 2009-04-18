e41f82a3d1b3674597ca12be6925849ad8231ace
Randy Schmidt <randy@umlatte.com>
2009-04-18 14:28:46 -0400

    Stub out helpers for sqlite3 so it can be used in a test environment

--- lib/puffer_fish/migration_helpers.rb
+++ lib/puffer_fish/migration_helpers.rb
@@ -17,7 +17,6 @@ module PufferFish
           key_name = "#{fk_table}_#{pk_table.to_s.singularize}_id_fkey"
           execute("ALTER TABLE #{fk_table} DROP FOREIGN KEY #{key_name};")
         end
-
       end
     end
 
@@ -31,7 +30,18 @@ module PufferFish
         def remove_foreign_key(fk_table, pk_table)
           execute("ALTER TABLE #{fk_table} DROP CONSTRAINT #{fk_table}_#{pk_table.to_s.singularize}_id_fkey;")
         end
+      end
+    end
+    
+    module SQLite3
+      module ClassMethods
+        def add_foreign_key(fk_table, pk_table, opts={})
+          # do nothing for now... this is just so that it works with sqlite3
+        end
 
+        def remove_foreign_key(fk_table, pk_table)
+          # do nothing for now... this is just so that it works with sqlite3
+        end
       end
     end
   end
